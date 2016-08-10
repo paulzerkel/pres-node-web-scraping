c7f22383ecdc2d0bfb17e876e1786cd75498b9fd
Paul Zerkel <zerkel@gmail.com>
2016-08-09 22:54:14 -0500

    A simple example of cheerio

--- examples/dom/index.js
+++ examples/dom/index.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const got = require('got');
+const cheerio = require('cheerio');
+
+got('localhost:8080/about', {
+  headers: {
+    'User-Agent': 'nodejstl-bot',
+  }})
+  .then(response => {
+    let $ = cheerio.load(response.body);
+    /*
+    $('table.list > tr').each((index, element) => {
+
+    });
+    */
+    let address = $('address');
+    if(address.length) {
+      console.log(address.text());
+    }
+
+    //console.log($('address').text())
+  })
+  .catch(error => {
+    console.log(error)
+  });
